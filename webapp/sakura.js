231157866740a29539e95879e0190976e87fbc15
zenjiro <zenjiro0123@gmail.com>
2013-11-29 11:38:25 +0900

    読み込んだデータを描画

--- webapp/sakura.js
+++ webapp/sakura.js
@@ -1,24 +1,6 @@
 function initialize() {
 	var myLatlng = new google.maps.LatLng(35.560981, 139.718344); // アロマスクエア
-	var images = {
-		modified : true,
-		images : [ {
-			lat : 35.560981,
-			lng : 139.718344,
-			img : "photo-600.jpg"
-		}, // アロマスクエア
-				{
-					lat : 35.562479,
-					lng : 139.716051,
-					img : "photo-600.jpg"
-				}, // JR蒲田
-				{
-					lat : 35.560685,
-					lng : 139.723731,
-					img : "photo-600.jpg"
-				} // 京急蒲田
-		]
-	};
+	var images = {};
 	var mapOptions = {
 		center : myLatlng,
 		zoom : 17,
@@ -39,32 +21,45 @@ function initialize() {
 	});
 	var done = {};
 	$(function() {
-		setInterval(function() {
-			$.get("http://kumano.zetta.flab.fujitsu.co.jp/sakura/photos.json",
-					function(data) {
-						console.log("データ：" + data);
-						// TODO dataをimagesに格納
-				});
-			// TODO データ読み込み完了のタイミングで実行
-				images.images.forEach(function(element) {
-					if (!done[element.lat + "_" + element.lng]) {
-						done[element.lat + "_" + element.lng] = true;
-						var infowindow = new google.maps.InfoWindow( {
-							content : '<img src=\'' + element.img + '\'/>'
-						});
-						var marker = new google.maps.Marker( {
-							position : new google.maps.LatLng(element.lat,
-									element.lng),
-							icon : image
-						});
-						google.maps.event.addListener(marker, 'click',
-								function() {
-									infowindow.open(map, marker);
-								});
-						marker.setMap(map);
-					}
-				});
-			}, 2000);
+		setInterval(
+				function() {
+					$
+							.getJSON(
+									"http://kumano.zetta.flab.fujitsu.co.jp/sakura/photos.json",
+									function(data) {
+										images.images = data.images;
+										images.images
+												.forEach(function(element) {
+													if (!done[element.lat + "_"
+															+ element.lng]) {
+														done[element.lat + "_"
+																+ element.lng] = true;
+														var infowindow = new google.maps.InfoWindow(
+																{
+																	content : '<img src=\'' + element.img + '\'/>'
+																});
+														var marker = new google.maps.Marker(
+																{
+																	position : new google.maps.LatLng(
+																			element.lat,
+																			element.lng),
+																	icon : image
+																});
+														google.maps.event
+																.addListener(
+																		marker,
+																		'click',
+																		function() {
+																			infowindow
+																					.open(
+																							map,
+																							marker);
+																		});
+														marker.setMap(map);
+													}
+												});
+									});
+				}, 2000);
 	});
 }
 google.maps.event.addDomListener(window, 'load', initialize);
